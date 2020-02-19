85e0270ded0a748964890e7d3ac8e6f83bba0c4f
Paul Theceri <paul.theceri@gmail.com>
2020-02-19 13:28:44 +0300

    cleaned M-Weza writings from the dashboard

--- resources/views/online_checkout/index.blade.php
+++ resources/views/online_checkout/index.blade.php
@@ -66,22 +66,22 @@ Endeleza: Online Checkouts
           </div> -->
           <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body dashboard-tabs p-0">
-                  <ul class="nav nav-tabs px-4" role="tablist">
+              <!-- <div class="card">
+                <div class="card-body dashboard-tabs p-0"> -->
+                  <!-- <ul class="nav nav-tabs px-4" role="tablist">
                     <li class="nav-item">
                       <a class="nav-link active" id="overview-tab" data-toggle="tab" href="#overview" role="tab" aria-controls="overview" aria-selected="true">Overview</a>
-                    </li>
+                    </li> -->
                     <!-- <li class="nav-item">
                       <a class="nav-link" id="sales-tab" data-toggle="tab" href="#sales" role="tab" aria-controls="sales" aria-selected="false">Sales</a>
                     </li>
                     <li class="nav-item">
                       <a class="nav-link" id="purchases-tab" data-toggle="tab" href="#purchases" role="tab" aria-controls="purchases" aria-selected="false">Purchases</a>
                     </li> -->
-                  </ul>
+                  <!-- </ul>
                   <div class="tab-content py-0 px-0">
                     <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview-tab">
-                      <div class="d-flex flex-wrap justify-content-xl-between">
+                      <div class="d-flex flex-wrap justify-content-xl-between"> -->
                         <!-- <div class="d-none d-xl-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
                           <i class="mdi mdi-calendar-heart icon-lg mr-3 text-primary"></i>
                           <div class="d-flex flex-column justify-content-around">
@@ -98,37 +98,37 @@ Endeleza: Online Checkouts
                             </div>
                           </div>
                         </div> -->
-                        <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                          <!-- <i class="mdi mdi-currency-usd mr-3 icon-lg text-danger"></i> -->
+                        <!-- <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                          <i class="mdi mdi-currency-usd mr-3 icon-lg text-danger"></i>
                           <div class="d-flex flex-column justify-content-around">
                             <small class="mb-1 text-muted">Number of Stalls</small>
                             <h5 class="mr-2 mb-0">532</h5>
                           </div>
-                        </div>
-                        <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                          <!-- <i class="mdi mdi-eye mr-3 icon-lg text-success"></i> -->
+                        </div> -->
+                        <!-- <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                          <i class="mdi mdi-eye mr-3 icon-lg text-success"></i>
                           <div class="d-flex flex-column justify-content-around">
                             <small class="mb-1 text-muted">Number of routes</small>
                             <h5 class="mr-2 mb-0">43</h5>
                           </div>
-                        </div>
-                        <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                          <!-- <i class="mdi mdi-download mr-3 icon-lg text-warning"></i> -->
+                        </div> -->
+                        <!-- <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                          <i class="mdi mdi-download mr-3 icon-lg text-warning"></i>
                           <div class="d-flex flex-column justify-content-around">
                             <small class="mb-1 text-muted">Number of deliveries completed</small>
                             <h5 class="mr-2 mb-0">13784</h5>
                           </div>
-                        </div>
-                        <div class="d-flex py-3 border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                          <!-- <i class="mdi mdi-flag mr-3 icon-lg text-danger"></i> -->
+                        </div> -->
+                        <!-- <div class="d-flex py-3 border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                          <i class="mdi mdi-flag mr-3 icon-lg text-danger"></i>
                           <div class="d-flex flex-column justify-content-around">
                             <small class="mb-1 text-muted">Number of deliveries paid for using cash / M-Pesa (no loan)</small>
                             <h5 class="mr-2 mb-0">8472</h5>
                           </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="tab-pane fade" id="sales" role="tabpanel" aria-labelledby="sales-tab">
+                        </div> -->
+                      <!-- </div> -->
+                    <!-- </div> -->
+                    <!-- <div class="tab-pane fade" id="sales" role="tabpanel" aria-labelledby="sales-tab">
                       <div class="d-flex flex-wrap justify-content-xl-between">
                         <div class="d-none d-xl-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
                           <i class="mdi mdi-calendar-heart icon-lg mr-3 text-primary"></i>
@@ -226,7 +226,7 @@ Endeleza: Online Checkouts
                     </div>
                   </div>
                 </div>
-              </div>
+              </div> -->
             </div>
           </div>
           <!-- <div class="row">
