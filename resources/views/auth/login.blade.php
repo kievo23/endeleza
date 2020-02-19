0af49ab59e4c2057a3155b19537085bcbc62ec61
Paul Theceri <paul.theceri@gmail.com>
2020-02-19 21:09:33 +0300

    changed the logo on the login page to Endeleza's

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header text-center">
-                    <img class="img-radius" src="\assets\images\logo.png">
+                    <img class="img-radius" src="\assets\images\Endeleza.png">
                 </div>
 
                 <div class="card-body">
