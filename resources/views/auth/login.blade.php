8f5967529e846294f078ac631fd8e19e34fda9d7
Kelvin Chege M <kelvinchege@gmail.com>
2021-05-08 21:12:36 +0300

    login left align remove

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@ Endeleza: Login
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header text-center">
-                    <img style="border-radius: 25px;position:relative; left: 54px;" class="img-radius" src="\assets\images\Endeleza.png">
+                    <img style="border-radius: 25px;position:relative;" class="img-radius" src="\assets\images\Endeleza.png">
                 </div>
 
                 <div class="card-body">
