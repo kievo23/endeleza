a74e563f219c3d23edebc92be1756fbd091e5af0
Kelvin Chege M <kelvinchege@gmail.com>
2021-05-13 17:11:16 +0300

    The report responsiveness

--- resources/views/dashboard/report.blade.php
+++ resources/views/dashboard/report.blade.php
@@ -15,31 +15,37 @@ Endeleza: Report
 
 @section('content')
 
-<!-- partial -->
-<div class="main-panel">
-  <div class="content-wrapper">
-    <div class="row">
-      <div class="col-md-12 grid-margin">
-        <div class="d-flex justify-content-between flex-wrap">
-          <div class="d-flex align-items-end flex-wrap">
-            <div class="mr-md-3 mr-xl-5">
-              <h2>Welcome back to the Endeleza Platform Summary</h2>
-              <p class="mb-md-0">Your analytics dashboard.</p>
-              <i class="mdi mdi-home text-muted hover-cursor"></i>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="d-flex justify-content-between flex-wrap">
+                <div class="d-flex align-items-end flex-wrap">
+                  <div class="mr-md-3 mr-xl-5">
+                    <h2>Report</h2>
+                    <p class="mb-md-0">Your analytics dashboard</p>
+                  </div>
+                  <div class="d-flex">
+                    <i class="mdi mdi-home text-muted hover-cursor"></i>
+                    <!-- <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
+                    <p class="text-primary mb-0 hover-cursor">Analytics</p> -->
+                  </div>
+                </div>
+                <div class="d-flex justify-content-between align-items-end flex-wrap">
+
+                </div>
+              </div>
             </div>
           </div>
+          <div class="row">
+            <div class="col-md-12 stretch-card">
+              <div class="card">
+                <div class="card-body">
 
-          <div class="">
-            <div class="main-body">
-              <div class="page-wrapper">
-                <div class="page-body">
-                  <div class="row">
-                    <div class="card col-xl-12 p-2 col-md-12">
-                      <div class="card-header">
-                        <h4>Books of Accounts</h4>
-                      </div>
-                      <div class="card-block">
-                        <div class="table-responsive">
+                  <hr>
+                  <div class="table-responsive">
                             <table class="table display" id="report">
                                 <thead class="success">
                                     <th>Year</th>
@@ -74,25 +80,12 @@ Endeleza: Report
                                 @endforeach
                                 </tbody>
                             </table>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-xl-11 col-md-11 p-2">
-                    <canvas id="myChart" width="400" height="100"></canvas>
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
-      </div>
-    </div>
-
-  </div>
   <!-- content-wrapper ends -->
 
   @endsection
