c5a2fcf80316f85c5e98d03afc78e0b1f80aac2d
Kelvin Chege M <kelvinchege@gmail.com>
2021-05-19 13:03:19 +0300

    word day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,9 +7,9 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
+    /** 
      * The Artisan commands provided by your application.
-     *
+     * 
      * @var array
      */
     protected $commands = [
