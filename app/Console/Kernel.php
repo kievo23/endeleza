e0ca72b62d0f1501b75c8af940fb60ae518072fe
Kelvin Chege M <kelvinchege@gmail.com>
2021-05-31 00:22:14 +0300

    changes to cron time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //         ->hourly();
-        $schedule->command('count_days:run')->hourlyAt(4);
+        $schedule->command('count_days:run')->hourlyAt(25);
         //$schedule->command('word:day')->everyMinute();
     }
 
