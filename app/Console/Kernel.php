12f64ec9fcddb66466d1328606094af57472adc3
Kelvin Chege M <kelvinchege@gmail.com>
2021-05-31 21:17:38 +0300

    test crontab

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //         ->hourly();
-        $schedule->command('count_days:run')->hourlyAt(15);
+        $schedule->command('count_days:run')->hourlyAt(20);
         //$schedule->command('word:day')->everyMinute();
     }
 
