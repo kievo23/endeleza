3aeb6aefa155b35b96dbee3aa3a7f136dba2f0d7
Kelvin Chege M <kelvinchege@gmail.com>
2021-07-14 20:55:50 +0300

    changes to late loans

--- resources/views/dashboard/welcome.blade.php
+++ resources/views/dashboard/welcome.blade.php
@@ -117,8 +117,8 @@ Endeleza: Dashboard
                           <div class="row align-items-end">
                             <div class="col-8">
                               <h5 class="text-white">{{ number_format($lateLoans, 2, '.', ',')}}</h5>
-                              <h6 class="text-white m-b-0">Value of Late Loans 8-29days</h6>
-                              <span>6 days {{$oneWeek}} | 2 weeks {{$twoWeeks}}</span>
+                              <h6 class="text-white m-b-0">Late Loans 8-29days</h6><br>
+                              <span>6 days {{number_format($oneWeek, 2, '.', ',')}} | 2 weeks {{number_format($twoWeeks, 2, '.', ',')}}</span>
                             </div>
                             <div class="col-4 text-right"><iframe class="chartjs-hidden-iframe" tabindex="-1" style="display: block; overflow: hidden; border: 0px; margin: 0px; top: 0px; left: 0px; bottom: 0px; right: 0px; height: 100%; width: 100%; position: absolute; pointer-events: none; z-index: -1;"></iframe><iframe class="chartjs-hidden-iframe" tabindex="-1" style="display: block; overflow: hidden; border: 0px; margin: 0px; top: 0px; left: 0px; bottom: 0px; right: 0px; height: 100%; width: 100%; position: absolute; pointer-events: none; z-index: -1;"></iframe>
                               <canvas id="update-chart-2" height="50" width="89" style="display: block; width: 89px; height: 50px;"></canvas>
