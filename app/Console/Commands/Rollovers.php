505731ea0fe16206599ed84bb1500a12ce349618
Kelvin Chege M <kelvinchege@gmail.com>
2022-01-13 11:52:43 +0300

    changes to ujumbe sms

--- app/Console/Commands/Rollovers.php
+++ app/Console/Commands/Rollovers.php
@@ -48,40 +48,40 @@ class Rollovers extends Command
         $loans = LoanAccount::where('loan_status','0')->get();
         //print_r($loans);
         foreach ($loans as $key => $loan) {
-            if($loan->customer->interest == 6 && $loan->days_in_arrears >= 8 && $loan->customer->rollover == 1){
-                //Daily interest
-                $daily_interest_rate = 0.86;
-                $daily_interest = $daily_interest_rate/100*$loan->loan_balance;
-                $new_loan_balance = $loan->loan_balance + $daily_interest;
-                $sms = "Dear customer, your OVERDUE loan has attracted Ksh. ". $daily_interest ." as lateness fees. Your new outstanding balance is Ksh. ".$new_loan_balance;
-                $res = SMS::sendSmsLeopard($loan->customer->customer_account_msisdn,$sms);
-                Outbox::log(json_decode($res),$sms);
-                //Log::alert($res);
-                $loan->loan_balance = $new_loan_balance;
-                $loan->loan_penalty = $loan->loan_penalty + $daily_interest;
-                $loan->save();
-            }
+            // if($loan->customer->interest == 6 && $loan->days_in_arrears >= 8 && $loan->customer->rollover == 1){
+            //     //Daily interest
+            //     $daily_interest_rate = 0.86;
+            //     $daily_interest = $daily_interest_rate/100*$loan->loan_balance;
+            //     $new_loan_balance = $loan->loan_balance + $daily_interest;
+            //     $sms = "Dear customer, your OVERDUE loan has attracted Ksh. ". $daily_interest ." as lateness fees. Your new outstanding balance is Ksh. ".$new_loan_balance;
+            //     $res = SMS::sendSmsLeopard($loan->customer->customer_account_msisdn,$sms);
+            //     Outbox::log(json_decode($res),$sms);
+            //     //Log::alert($res);
+            //     $loan->loan_balance = $new_loan_balance;
+            //     $loan->loan_penalty = $loan->loan_penalty + $daily_interest;
+            //     $loan->save();
+            // }
 
-            if($loan->customer->interest == 10.5 && $loan->days_in_arrears >= 15 && $loan->customer->rollover == 1){
-                //Daily interest
-                $daily_interest_rate = 0.75;
-                $daily_interest = $daily_interest_rate/100*$loan->loan_balance;
-                $new_loan_balance = $loan->loan_balance + $daily_interest;
-                $sms = "Dear customer, your OVERDUE loan has attracted Ksh. ". $daily_interest ." as lateness fees. Your new outstanding balance is Ksh. ".$new_loan_balance;
-                $res = SMS::sendSmsLeopard($loan->customer->customer_account_msisdn,$sms);
-                Outbox::log(json_decode($res),$sms);
-                //Log::alert($res);
-                $loan->loan_balance = $new_loan_balance;
-                $loan->loan_penalty = $loan->loan_penalty + $daily_interest;
-                $loan->save();
-            }
+            // if($loan->customer->interest == 10.5 && $loan->days_in_arrears >= 15 && $loan->customer->rollover == 1){
+            //     //Daily interest
+            //     $daily_interest_rate = 0.75;
+            //     $daily_interest = $daily_interest_rate/100*$loan->loan_balance;
+            //     $new_loan_balance = $loan->loan_balance + $daily_interest;
+            //     $sms = "Dear customer, your OVERDUE loan has attracted Ksh. ". $daily_interest ." as lateness fees. Your new outstanding balance is Ksh. ".$new_loan_balance;
+            //     $res = SMS::sendSmsLeopard($loan->customer->customer_account_msisdn,$sms);
+            //     Outbox::log(json_decode($res),$sms);
+            //     //Log::alert($res);
+            //     $loan->loan_balance = $new_loan_balance;
+            //     $loan->loan_penalty = $loan->loan_penalty + $daily_interest;
+            //     $loan->save();
+            // }
 
             $dayOfTheWeek = Carbon::now()->dayOfWeek;
             $sms = new Ujumbe(config('app.SMS_UJUMBE_TOKEN'), config('app.SMS_UJUMBE_EMAIL'));
 
             
             //Late Payment Reminders
-            if($loan->customer->interest == 6 && $loan->days_in_arrears >= 7 && ($dayOfTheWeek == 1 || $dayOfTheWeek == 3 || $dayOfTheWeek == 5)){
+            if($loan->customer->interest == 6 && $loan->days_in_arrears >= 7 && ($dayOfTheWeek == 1 || $dayOfTheWeek == 4 || $dayOfTheWeek == 5)){
                 $new_amt = $loan->loan_balance + $loan->loan_penalty;
                 $sms = "Dear Customer, your loan balance of Ksh. ".$new_amt." is in Default!. Lipa mdogo mdogo to clear and get a new stock. Till Number 5041363";
                 $sms->send($loan->customer->customer_account_msisdn, $sms, "EndelezaCap");
@@ -90,7 +90,7 @@ class Rollovers extends Command
                 Outbox::log(json_decode($res),$sms);                
             }
 
-            if($loan->customer->interest == 10.5 && $loan->days_in_arrears >= 14 && ($dayOfTheWeek == 1 || $dayOfTheWeek == 3 || $dayOfTheWeek == 5)){
+            if($loan->customer->interest == 10.5 && $loan->days_in_arrears >= 14 && ($dayOfTheWeek == 1 || $dayOfTheWeek == 4 || $dayOfTheWeek == 5)){
                 $new_amt = $loan->loan_balance + $loan->loan_penalty;
                 $sms = "Dear Customer, your loan balance of Ksh. ".$new_amt." is in Default!. Lipa mdogo mdogo to clear and get a new stock. Till Number 5041363";
                 $sms->send($loan->customer->customer_account_msisdn, $sms, "EndelezaCap");
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //         ->hourly();
         $schedule->command('count_days:run')->hourlyAt(5);
-        $schedule->command('rollovers:run')->dailyAt('15:20');
+        $schedule->command('rollovers:run')->dailyAt('11:55');
         //$schedule->command('word:day')->everyMinute();
     }
 
