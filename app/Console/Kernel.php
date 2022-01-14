b2532a5cc940f4f99483d9ee75c51b67d718ef43
Kelvin Chege M <kelvinchege@gmail.com>
2022-01-14 13:52:19 +0300

    changes to fix ujumbe sms configuration

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //         ->hourly();
         $schedule->command('count_days:run')->hourlyAt(5);
-        $schedule->command('rollovers:run')->dailyAt('16:03');
+        $schedule->command('rollovers:run')->dailyAt('14:10');
         //$schedule->command('word:day')->everyMinute();
     }
 
